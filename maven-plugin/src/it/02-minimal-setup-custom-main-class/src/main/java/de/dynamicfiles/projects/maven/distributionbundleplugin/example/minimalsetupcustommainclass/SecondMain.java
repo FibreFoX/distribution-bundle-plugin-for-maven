e1745fb31d67f6490d6afe3ec2bcbbd5fbb5055f
FibreFoX <fibrefox@dynamicfiles.de>
2018-04-02 19:29:26 +0200

    added invoker-plugin for having test-projects

---
+++ maven-plugin/src/it/02-minimal-setup-custom-main-class/src/main/java/de/dynamicfiles/projects/maven/distributionbundleplugin/example/minimalsetupcustommainclass/SecondMain.java
@@ -0,0 +1,9 @@
+package de.dynamicfiles.projects.maven.distributionbundleplugin.example.minimalsetupcustommainclass;
+
+public class SecondMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World from second main class.");
+    }
+
+}
