2c8b6ced8347d4524eee7718e3bd49c2c9c254c0
FibreFoX <fibrefox@dynamicfiles.de>
2018-04-02 20:58:44 +0200

    added it project

---
+++ maven-plugin/src/it/03-minimal-setup-without-prior-main-class/src/main/java/de/dynamicfiles/projects/maven/distributionbundleplugin/example/minimalsetupwithoutpriormainclass/Main.java
@@ -0,0 +1,9 @@
+package de.dynamicfiles.projects.maven.distributionbundleplugin.example.minimalsetupwithoutpriormainclass;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
