617e7bcf7bd3f8743dbb89e7ba20fd1c5e355be3
FibreFoX <fibrefox@dynamicfiles.de>
2018-04-02 22:58:59 +0200

    added simple spring boot example

---
+++ maven-plugin/src/it/04-simple-spring-boot-setup/src/main/java/de/dynamicfiles/projects/maven/distributionbundleplugin/example/simplespringbootsetup/MainEntrypoint.java
@@ -0,0 +1,23 @@
+package de.dynamicfiles.projects.maven.distributionbundleplugin.example.simplespringbootsetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class MainEntrypoint {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainEntrypoint.class, args);
+    }
+
+}
