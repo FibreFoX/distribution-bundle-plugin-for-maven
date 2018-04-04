2600a6309502fac3e2a4c04715b0c0ff88f14ee2
FibreFoX <fibrefox@dynamicfiles.de>
2018-04-04 17:16:41 +0200

    added methods to check 32bit or 64bit of windows/linux executable

--- maven-plugin/src/main/java/de/dynamicfiles/projects/maven/distributionbundleplugin/InternalUtils.java
+++ maven-plugin/src/main/java/de/dynamicfiles/projects/maven/distributionbundleplugin/InternalUtils.java
@@ -3,15 +3,21 @@ package de.dynamicfiles.projects.maven.distributionbundleplugin;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.ByteOrder;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.file.FileVisitResult;
 import java.nio.file.FileVisitor;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.StandardCopyOption;
+import java.nio.file.StandardOpenOption;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.jar.JarFile;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -124,5 +130,86 @@ public class InternalUtils {
 
     public boolean isPlatformWindows() {
         return System.getProperty("os.name").toLowerCase().contains("win");
+    }
+
+    public boolean isLinuxExecutable64bit(Path linuxBinary) {
+        try(FileChannel openFileChannel = FileChannel.open(linuxBinary, StandardOpenOption.READ)){
+            MappedByteBuffer startMarker = openFileChannel.map(FileChannel.MapMode.READ_ONLY, 0, 4);
+            startMarker.load();
+            if( startMarker.get(0) == 0x7F && startMarker.get(1) == 'E' && startMarker.get(2) == 'L' && startMarker.get(3) == 'F' ){
+                // e_machine inside Elf*_Ehdr
+                MappedByteBuffer architectureBytes = openFileChannel.map(FileChannel.MapMode.READ_ONLY, 18, 2);
+                architectureBytes.load();
+                short architectureShort = architectureBytes.getShort();
+                // https://stackoverflow.com/a/7932774/1961102
+                int architectureAsInt = architectureShort >= 0 ? architectureShort : 0x10000 + architectureShort;
+
+                // http://www.sco.com/developers/gabi/1998-04-29/ch4.eheader.html
+                // https://en.wikipedia.org/wiki/Executable_and_Linkable_Format#File_header
+                // 32 bit
+                if( architectureAsInt == 0x03 ){
+                    return false;
+                }
+
+                // 64 bit (x86-64)
+                if( architectureAsInt == 0x3E ){
+                    return true;
+                }
+            }
+
+        } catch(IOException ex){
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+
+    public boolean isWindowsExecutable64bit(Path windowsBinary) {
+        // try to find PE-header offset inside MS-DOS header and check architecture
+        try(FileChannel openFileChannel = FileChannel.open(windowsBinary, StandardOpenOption.READ)){
+            MappedByteBuffer startMarker = openFileChannel.map(FileChannel.MapMode.READ_ONLY, 0, 2);
+            startMarker.load();
+            if( startMarker.get(0) == 'M' && startMarker.get(1) == 'Z' ){
+                // found MS-DOS header magic bytes
+
+                // MS-DOS header contains 4 byte PE-offset at 0x60 (e_lfanew in MZ structure)
+                MappedByteBuffer offsetMarker = openFileChannel.map(FileChannel.MapMode.READ_ONLY, 60, 4);
+                offsetMarker.load();
+                offsetMarker.order(ByteOrder.LITTLE_ENDIAN);
+                int foundOffset = offsetMarker.getInt();
+                // make it positive in case of huge offset-gap 
+                foundOffset = foundOffset >= 0 ? foundOffset : 0x10000 + foundOffset;
+                // TODO put into log on verbose
+                System.out.println("Found PE-Header offset: " + foundOffset);
+
+                if( foundOffset > 0 ){
+                    // found PE header offset, reading the next 6 bytes should be enough to verify
+                    MappedByteBuffer peHeader = openFileChannel.map(FileChannel.MapMode.READ_ONLY, foundOffset, 6);
+                    peHeader.load();
+                    peHeader.order(ByteOrder.LITTLE_ENDIAN);
+                    if( peHeader.get(0) == 'P' && peHeader.get(1) == 'E' && peHeader.get(2) == '\0' && peHeader.get(3) == '\0' ){
+                        // TODO put into log on verbose
+                        // found legal PE header, now reading architecture
+                        short architectureShort = peHeader.getShort(4);
+                        // https://stackoverflow.com/a/7932774/1961102
+                        int architectureAsInt = architectureShort >= 0 ? architectureShort : 0x10000 + architectureShort;
+
+                        // https://msdn.microsoft.com/library/windows/desktop/ms680547(v=vs.85).aspx?id=19509#machine_types
+                        // 32bit
+                        if( architectureAsInt == 0x014c ){
+                            return false;
+                        }
+                        // 64bit
+                        if( architectureAsInt == 0x8664 ){
+                            return true;
+                        }
+                        // TODO put into log on verbose: different/unsupported architecture
+                    }
+                }
+            }
+        } catch(IOException ex){
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return false;
     }
 }
