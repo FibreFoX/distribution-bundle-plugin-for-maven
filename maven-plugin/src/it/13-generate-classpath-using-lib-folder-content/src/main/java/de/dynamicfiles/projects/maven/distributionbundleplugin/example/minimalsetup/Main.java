826bfffe485895d2d86611140c6aa1388ee18f8f
FibreFoX <fibrefox@dynamicfiles.de>
2018-06-17 18:21:41 +0200

    added example project for `generateClasspathUsingLibFolder`-feature

---
+++ maven-plugin/src/it/13-generate-classpath-using-lib-folder-content/src/main/java/de/dynamicfiles/projects/maven/distributionbundleplugin/example/minimalsetup/Main.java
@@ -0,0 +1,9 @@
+package de.dynamicfiles.projects.maven.distributionbundleplugin.example.minimalsetup;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
